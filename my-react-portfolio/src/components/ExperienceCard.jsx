e51e83d6f00ec65797ef33cb821cc9294261de77
Marcelo <cerealkill05@gmail.com>
2025-05-24 23:16:02 -0500

    again.

--- my-react-portfolio/src/components/ExperienceCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function ExperienceCard({ title }) {
-  return <div className="card">{title}</div>;
-}
-
-export default ExperienceCard;
-// This is a simple experience card component for a portfolio website.
--- my-react-portfolio/src/components/ServiceCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function ServiceCard({ name }) {
-  return <div className="card">{name}</div>;
-}
-
-export default ServiceCard;
-// This is a simple service card component for a portfolio website.
